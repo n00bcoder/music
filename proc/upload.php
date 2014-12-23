ce8b58d9cef7e3af42552edc301939c9ff0875a9
Grant Schmidt <grant@grantschmidt.net>
2014-12-23 01:24:32 -0800

    update notes and upload.php
    Changed upload.php to upload into the music folder, and added the notes
    on what needs fixing still, and what needs to be finished up

--- proc/upload.php
+++ proc/upload.php
@@ -9,7 +9,7 @@ if (!$fileTmpLoc) {
     // if file not chosen 
     echo "ERROR: Please browse for a file before clicking the upload button."; exit(); 
 } 
-if(move_uploaded_file($fileTmpLoc, "../progresstest/$fileName")){ 
+if(move_uploaded_file($fileTmpLoc, "../music/$fileName")){ 
     echo "$fileName upload is complete"; 
 } else { 
     echo "move_uploaded_file function failed"; } 
